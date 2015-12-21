b27ea2f0eb608fad3a6320deb42a69624cc77fa8
Gaël Rottier <gaelrottier@gmail.com>
2015-12-21 13:23:27 +0100

    MainItemAdaptor + layout main activity

---
+++ app/src/main/java/model/MainItemAdaptor.java
@@ -0,0 +1,65 @@
+package model;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.androidquery.AQuery;
+
+import java.util.List;
+
+import fr.unice.mbds.maslow.R;
+
+/**
+ * Created by Gael on 20/12/2015.
+ */
+public class MainItemAdaptor extends BaseAdapter {
+    private Context context;
+    private List<Class<? extends AppCompatActivity>> boutons;
+
+    public MainItemAdaptor(Context context, List<Class<? extends AppCompatActivity>> boutons) {
+        this.context = context;
+        this.boutons = boutons;
+    }
+
+    @Override
+    public int getCount() {
+        return boutons.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return boutons.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = View.inflate(context, R.layout.activity_main_list_item, null);
+        }
+
+        AQuery aq = new AQuery(convertView);
+
+        aq.id(convertView.findViewById(R.id.activity_main_img_view)).image(R.drawable.fff);
+
+        aq.id(convertView).clicked(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, boutons.get(position));
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                context.startActivity(i);
+            }
+        });
+
+        return convertView;
+    }
+}
