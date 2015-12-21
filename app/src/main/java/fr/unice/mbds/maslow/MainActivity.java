f1dc4b9a8b6167fe0a26c4413ce4a9c9315b4964
Gaël Rottier <gaelrottier@gmail.com>
2015-12-21 13:31:14 +0100

    Affichage des boutons sur le main (à vérifier, je n'ai pas de cable à portée pour tester)

--- app/src/main/java/fr/unice/mbds/maslow/MainActivity.java
+++ app/src/main/java/fr/unice/mbds/maslow/MainActivity.java
@@ -3,15 +3,33 @@ package fr.unice.mbds.maslow;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.MainItemAdaptor;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView gridViewListeBoutons;
+    private MainItemAdaptor adaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gridViewListeBoutons = (GridView) findViewById(R.id.activity_main_grid_view);
+
+        List<Class<? extends AppCompatActivity>> listeBoutons = new ArrayList<>();
+
+        listeBoutons.add(PlayListsActivity.class);
+        listeBoutons.add(ListeMusiquesActivity.class);
+
+        adaptor = new MainItemAdaptor(getApplicationContext(), listeBoutons);
+
         //start playlist activity
-        startActivity(new Intent(this,PlayListsActivity.class));
+        //startActivity(new Intent(this, PlayListsActivity.class));
     }
 }
