e19c9c53844dc4ee161d0892630c4b43f06cec94
zac_simo <zsimale@gmail.com>
2016-01-08 23:15:47 +0100

    modifications à la création dynamique de la liste de Playlist

---
+++ app/src/main/java/adapters/PlayListItemAdapter.java
@@ -0,0 +1,84 @@
+package adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Toast;
+
+import com.androidquery.AQuery;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import fr.unice.mbds.maslow.R;
+import object.Musique;
+
+/**
+ * Created by Zac on 08/01/2016.
+ */
+public class PlayListItemAdapter extends BaseAdapter{
+    private Context context;
+    private List<Class<? extends AppCompatActivity>> boutons;
+    private Musique musique;
+    private List<Musique> musiqueList = new ArrayList<>();
+
+    public PlayListItemAdapter(Context applicationContext, List<Class<? extends AppCompatActivity>> boutons) {
+        this.context = applicationContext;
+        this.boutons = boutons;
+    }
+
+    @Override
+    public int getCount() {
+        return boutons.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return boutons.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = View.inflate(context, R.layout.layout_play_list_item, null);
+        }
+        AQuery aq = new AQuery(convertView);
+
+
+        aq.id(convertView.findViewById(R.id.buttonPlayListItem)).text("PlayList_" + position).clicked(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(context, "PlayList_" + position, Toast.LENGTH_LONG);
+                toast.show();
+                Random r = new Random();
+                int max = r.nextInt(5) + 5;
+                musiqueList.clear();
+                for (int i = 0; i < max; i++) {
+                    musique = new Musique("musique_" + i, "Artiste_" + i);
+                    musiqueList.add(musique);
+                }
+                Intent intent = new Intent(context, boutons.get(position));
+                Bundle extra = new Bundle();
+                extra.putSerializable("playList", (Serializable) musiqueList);
+                intent.putExtra("extra", extra);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                context.startActivity(intent);
+            }
+        });
+        return convertView;
+    }
+
+
+}
--- app/src/main/java/fr/unice/mbds/maslow/PlayListsActivity.java
+++ app/src/main/java/fr/unice/mbds/maslow/PlayListsActivity.java
@@ -1,89 +1,37 @@
 package fr.unice.mbds.maslow;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.view.KeyEvent;
+import android.widget.ListView;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import object.Musique;
+import adapters.PlayListItemAdapter;
 
 public class PlayListsActivity extends AppCompatActivity {
-    protected Musique musique;
-    protected List<Musique> playListSalon, playListChambre, playlistSaleBain, playListCommune;
+    protected int nombrePlayList;
+    protected PlayListItemAdapter adapter;
+    protected ListView listView;
+    protected List<Class<? extends AppCompatActivity>> boutons = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_lists);
-        playListSalon = new ArrayList<>();
-        playListChambre = new ArrayList<>();
-        playlistSaleBain = new ArrayList<>();
-        playListCommune = new ArrayList<>();
-        Random random = new Random();
-        int rand = 0;
-        for (int i=0;i<10;i++){
-            musique = new Musique("musique_"+i, "Artiste_"+i);
-            playListSalon.add(musique);
-            musique = new Musique("musique_"+(i+7), "Artiste_"+(i+7));
-            playListChambre.add(musique);
-            musique = new Musique("musique_"+(i+15), "Artiste_"+(i+15));
-            playlistSaleBain.add(musique);
-            rand = random.nextInt(25);
-            musique = new Musique("musique_"+rand, "Artiste_"+rand);
-            playListCommune.add(musique);
+        setContentView(R.layout.activity_liste_play_list);
+        listView = (ListView) findViewById(R.id.listViewItemPlayList);
+
+        Random r = new Random();
+        List<Class<? extends AppCompatActivity>> boutons = new ArrayList<>();
+        nombrePlayList = (r.nextInt(5)) + 5;
+        for (int i=0;i<nombrePlayList;i++) {
+            boutons.add(ListeMusiquesActivity.class);
         }
-        Button btnPlayListSalon = (Button) findViewById(R.id.btn_playlist_salon);
-        Button btnPlayListChambre = (Button) findViewById(R.id.btn_playlist_chambre);
-        Button btnPlayListSaleBain = (Button) findViewById(R.id.btn_playlist_sbain);
-        Button btnPlayListCommune = (Button) findViewById(R.id.btn_playlist_commune);
 
-        btnPlayListSalon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PlayListsActivity.this,ListeMusiquesActivity.class);
-                Bundle extra = new Bundle();
-                extra.putSerializable("playList", (Serializable) playListSalon);
-                intent.putExtra("extra", extra);
-                startActivity(intent);
-            }
-        });
+        adapter = new PlayListItemAdapter(getApplicationContext(),boutons);
+        listView.setAdapter(adapter);
 
-        btnPlayListChambre.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PlayListsActivity.this,ListeMusiquesActivity.class);
-                Bundle extra = new Bundle();
-                extra.putSerializable("playList", (Serializable) playListChambre);
-                intent.putExtra("extra", extra);
-                startActivity(intent);
-            }
-        });
-
-        btnPlayListSaleBain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PlayListsActivity.this,ListeMusiquesActivity.class);
-                Bundle extra = new Bundle();
-                extra.putSerializable("playList", (Serializable) playlistSaleBain);
-                intent.putExtra("extra", extra);
-                startActivity(intent);
-            }
-        });
-
-        btnPlayListCommune.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PlayListsActivity.this,ListeMusiquesActivity.class);
-                Bundle extra = new Bundle();
-                extra.putSerializable("playList", (Serializable) playListCommune);
-                intent.putExtra("extra", extra);
-                startActivity(intent);
-            }
-        });
     }
+
 }
