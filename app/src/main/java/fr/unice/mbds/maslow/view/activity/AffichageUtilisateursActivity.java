a8cf10d53718dd55a8e8fca79eecb7105c6730a8
zac_simo <zsimale@gmail.com>
2016-03-27 18:32:36 +0200

    --

--- app/src/main/java/fr/unice/mbds/maslow/view/activity/AffichageUtilisateursActivity.java
+++ app/src/main/java/fr/unice/mbds/maslow/view/activity/AffichageUtilisateursActivity.java
@@ -30,8 +30,6 @@ public class AffichageUtilisateursActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affichage_utilisateurs);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
         listeUtilisateurs = (ListView)findViewById(R.id.listViewUtilisateurs);
 
--- app/src/main/java/fr/unice/mbds/maslow/view/adapter/UtilisateursItemAdapter.java
+++ app/src/main/java/fr/unice/mbds/maslow/view/adapter/UtilisateursItemAdapter.java
@@ -1,16 +1,22 @@
 package fr.unice.mbds.maslow.view.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 import com.androidquery.AQuery;
 
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import fr.unice.mbds.maslow.R;
 import fr.unice.mbds.maslow.entities.Utilisateur;
+import fr.unice.mbds.maslow.view.activity.DetailUtilisateurActivity;
 
 /**
  * Created by Zac on 21/03/2016.
@@ -40,7 +46,7 @@ public class UtilisateursItemAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         if(convertView == null){
             convertView = View.inflate(context, R.layout.layout_utilisateur_item,null);
         }
@@ -48,6 +54,20 @@ public class UtilisateursItemAdapter extends BaseAdapter {
         AQuery aq = new AQuery(convertView);
         aq.id(R.id.textViewNomUserList).text(utilisateurList.get(position).getNom());
         aq.id(R.id.textViewPrenomUserList).text(utilisateurList.get(position).getPrenom());
+        aq.id(R.id.buttonDetailUtilisateur).getButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailUtilisateurActivity.class);
+                Bundle extra = new Bundle();
+                Map<String, String> strings = new HashMap<String, String>();
+                strings.put("nom",utilisateurList.get(position).getNom());
+                strings.put("prenom",utilisateurList.get(position).getPrenom());
+                strings.put("identifiant",utilisateurList.get(position).getIdentifiant());
+                extra.putSerializable("detailUtilisateur", (Serializable) strings);
+                intent.putExtra("extra", extra);
+                context.startActivity(intent);
+            }
+        });
         return convertView;
     }
 }
